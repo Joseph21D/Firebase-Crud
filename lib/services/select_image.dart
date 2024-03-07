6dcb12a25fa51c85026da6de430f4c9a0f36382b
Joseph Diaz <jomadica2117@gmail.com>
2024-03-07 18:40:58 -0500

    Service getImage

--- lib/services/select_image.dart
+++ lib/services/select_image.dart
@@ -3,12 +3,6 @@ import 'package:image_picker/image_picker.dart';
 Future<XFile?> getImage() async {
   final ImagePicker picker = ImagePicker();
   final XFile? image = await picker.pickImage(source: ImageSource.gallery);
-  /*if (image != null) {
-    setState(() {
-      imageToUpload = File(image.path);
-    });
-  }*/
-
-
+  
   return image;
 }
